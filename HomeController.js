16a696ec8d727ddc1ae6043e3233a3f5f3fe7184
sola614 <36149249+sola614@users.noreply.github.com>
2018-08-23 11:19:47 +0800

    Update HomeController.js

--- HomeController.js
+++ HomeController.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
 module.exports = async function (ctx, next) {
   ctx.state.site = {
-    title:'硬之城 - 智能化元器件供应链服务平台',
-    keyword:'硬之城,元器件供应链',
-    description:'硬之城是基于大数据与人工智能的元器件供应链解决方案服务平台商城，提供元器件在线交易、项目风险控制及BOM管理等可视化供应链服务'
+    title:'',
+    keyword:'',
+    description:''
   };
   let cookies = ctx.cookies;
   let apiPath = ctx.state.apiPath;
