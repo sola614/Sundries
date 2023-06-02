8f64942d7f67c94e8f66a86bfbabaa154663fe88
sola614 <36149249+sola614@users.noreply.github.com>
2023-06-02 10:07:40 +0800

    Update aws-auto-shutdown.sh

--- course/aws-auto-shutdown.sh
+++ course/aws-auto-shutdown.sh
@@ -4,5 +4,5 @@ TRAFF_USED=$(vnstat --oneline b | awk -F';' '{print $11}')
 CHANGE_TO_GB=$(echo "scale=2; $TRAFF_USED / 1073741824" | bc) #如果系统没有bc请自行安装
 if (( $(echo "$CHANGE_TO_GB > $TRAFF_TOTAL" | bc -l) )); then
     shutdown -h now
-    curl https://api.meaqua.fun/api/sendMsg -X POST -d '{"msgType": 30,"msg": "服务器流量超标自动关机了","qqPojo":{}}' --header "Content-Type: application/json"
+    curl https://api.meaqua.fun/api/sendMsg -X POST -d '{"msgType": 30,"msg": "服务器流量超标自动关机了","qqPojo":{}}' --header "Content-Type: application/json" #消息通知
 fi
