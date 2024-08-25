9054d4dccacdcd3270aa611bbe92370f579ba3c2
sola614 <36149249+sola614@users.noreply.github.com>
2024-08-25 21:41:08 +0800

    Update install_docker.sh

--- shell/install_docker.sh
+++ shell/install_docker.sh
@@ -4,12 +4,17 @@
 check_command() {
   command -v "$1" >/dev/null 2>&1
 }
-
-# 安装 Docker 的函数
-docker_install() {
-  if ! check_command docker; then
-    echo "正在安装 Docker..."
-    curl -fsSL https://get.docker.com | bash
+# 判断是否是 Rocky
+is_rocky() {
+  if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    [ "$ID" = "rocky" ]
+  else
+    echo "无法检测操作系统版本。"
+    exit 1
+  fi
+}
+start_enable_docker(){
     echo "Docker 已安装完毕，正在启动..."
     systemctl start docker
 
@@ -33,10 +38,28 @@ docker_install() {
     echo "docker start/stop/restart/rm [CONTAINER ID or name]"
     echo "停止所有容器运行： docker stop \$(docker ps -a -q)"
     echo "删除所有停止运行的容器： docker rm \$(docker ps -a -q)"
-  else
-    echo "Docker 已经安装。"
-  fi
+}
+# 安装 Docker 的函数
+install() {
+    echo "正在安装 Docker..."
+    curl -fsSL https://get.docker.com | bash
+    start_enable_docker
+}
+
+rocky_install(){
+  sudo dnf check-update
+  sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+  sudo dnf install docker-ce docker-ce-cli containerd.io
+  start_enable_docker
 }
 
 # 开始安装 Docker
-docker_install
+if ! check_command docker; then
+  if is_rocky; then
+    rocky_install
+  else
+    install
+  fi
+else
+    echo "Docker 已经安装。"
+fi
